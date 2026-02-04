418115037035d80af723b9faf5d1e30696ebeeec
Connor Gibson <connor@nextdoortech.net>
2026-02-03 21:21:08 -0800

    Added additional test, fixed bounds overflow

--- Labs/Lab 02/maze_solver.py
+++ Labs/Lab 02/maze_solver.py
@@ -8,11 +8,11 @@ def maze_solver_helper(maze: list[list[int]], x: int, y: int, path: list[tuple])
         path.append((x, y))
         return True
 
-    elif x <= len(maze) and maze_solver_helper(maze, x+1, y, path):  # down
+    elif x < len(maze)-1 and maze_solver_helper(maze, x+1, y, path):  # down
         path.append((x, y))
         return True
 
-    elif y <= len(maze[0]) and maze_solver_helper(maze, x, y+1, path):  # right
+    elif y < len(maze[0])-1 and maze_solver_helper(maze, x, y+1, path):  # right
         path.append((x, y))
         return True
 
@@ -61,12 +61,12 @@ test_maze_2 = [
     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
     [1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
     [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # go left here
-    [0, 1, 1, 1, 1, 0, 1, 1, 1, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0], # dead end moving down
-    [0, 1, 0, 1, 1, 1, 1, 0, 1, 1],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [1, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+    [0, 1, 0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+    [1, 1, 0, 0, 0, 1, 1, 1, 1, 0],
 ]
 
 maze_solver(test_maze_2)
