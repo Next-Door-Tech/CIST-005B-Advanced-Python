9bf881f6284a36d5a19f63a01a64af6a5c101d26
Connor Gibson <connor@nextdoortech.net>
2026-03-01 21:18:59 -0800

    Move IdentitySet and FrozenIdentitySet to their own module

--- Labs/Lab 04/cards.py
+++ Labs/Lab 04/cards.py
@@ -6,7 +6,8 @@ from functools import singledispatchmethod
 from dataclasses import dataclass, asdict, InitVar, KW_ONLY
 from typing import Literal, ClassVar, overload, Self, Callable
 from collections import deque
-from collections.abc import MutableSequence, Set, MutableSet
+from collections.abc import MutableSequence
+from identityset import FrozenIdentitySet
 import re
 
 __all__ = ["Card", "Deck", "Shoe", "Hand"]
@@ -96,62 +97,6 @@ class OrderedEnum(Enum):  # From python docs
 
     def __lt__(self, other):
         return self.value < other.value if self.__class__ is other.__class__ else NotImplemented
-
-
-class FrozenIdentitySet[T](Set[T]):  # Adapted from https://stackoverflow.com/a/17039643/2565329
-    """A functional frozenset() implementation allowing for mutable members by using `id()` instead of `==`."""
-
-    __key = id
-
-    def __init__(self, iterable=()):
-        self.__map = {}
-        self.__map |= zip(map(self.__key, iterable), iterable)
-
-    def __len__(self):
-        return len(self.__map)
-
-    def __contains__(self, x):
-        return self.__key(x) in self.__map
-
-    def __iter__(self):
-        return iter(self.__map.values())
-
-    def __repr__(self):
-        if not self:
-            return f"{self.__class__.__name__!s}"
-        return f"{self.__class__.__name__!s}({list(self)!r})"
-
-
-class IdentitySet[T](MutableSet[T]):  # Adapted from https://stackoverflow.com/a/17039643/2565329
-    """A functional set() implementation allowing for mutable members by using `id()` instead of `==`."""
-
-    __key = id
-
-    def __init__(self, iterable=()):
-        self.__map = {}
-        self.__map |= zip(map(self.__key, iterable), iterable)
-
-    def __len__(self):
-        return len(self.__map)
-
-    def __contains__(self, x):
-        return self.__key(x) in self.__map
-
-    def __iter__(self):
-        return iter(self.__map.values())
-
-    def add(self, value):
-        """Add an element."""
-        self.__map[self.__key(value)] = value
-
-    def discard(self, value):
-        """Remove an element.  Do not raise an exception if absent."""
-        self.__map.pop(self.__key(value), None)
-
-    def __repr__(self):
-        if not self:
-            return f"{self.__class__.__name__!s}"
-        return f"{self.__class__.__name__!s}({list(self)!r})"
 
 
 class Rank(OrderedEnum):
