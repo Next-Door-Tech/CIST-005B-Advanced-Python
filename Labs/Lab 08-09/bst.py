dea5e125b39c44caa35a29d5b383d08331cd69ef
Connor Gibson <connor@nextdoortech.net>
2026-03-31 18:59:48 -0700

    Implement SimpleBST

--- Labs/Lab 08-09/bst.py
+++ Labs/Lab 08-09/bst.py
@@ -1,7 +1,6 @@
 from typing import Self
 
-
-class BinarySearchTree[T]:
+class SimpleBST[T]:
     class Node:
         def __init__(self, value: T, left=None, right=None) -> None:
             self.value: T = value
@@ -34,12 +33,15 @@ class BinarySearchTree[T]:
                 else:
                     self.right = self.__class__(value)
 
-        def delete(self, value: T) -> None:
-            if value == self.value:
+        # def delete(self, value: T) -> None:
+        #     if value == self.value:
+        #         if self.left is None:
+        #             self = self.right
 
+    root: Node | None
 
     def __init__(self):
-        self.root: Self | None = None
+        self.root = None
 
     def search(self, value: T) -> T:
         cur = self.root
@@ -63,34 +65,81 @@ class BinarySearchTree[T]:
             if value < cur.value:
                 if cur.left is None:
                     cur.left = self.Node(value)
+                    return
                 else:
                     cur = cur.left
             else:
                 if cur.right is None:
                     cur.right = self.Node(value)
+                    return
                 else:
                     cur = cur.right
 
     def delete(self, value: T) -> None:
-        cur = None
-        nxt = self.root
+        class NodeRef:
+            @property
+            def ref(self) -> SimpleBST.Node:
+                nonlocal cur, nxt_attr
+                return getattr(cur, nxt_attr)
 
-        while nxt is not None:
-            if value == nxt.value:
-                if cur is self.root:
+            @ref.setter
+            def ref(self, node: SimpleBST.Node) -> None:
+                nonlocal cur, nxt_attr
+                setattr(cur, nxt_attr, node)
 
-                    while cur.left is not None:
-                        cur.value = cur.right.value
+            @property
+            def value(self) -> T:
+                return self.ref.value
 
-                    if cur.
+            @value.setter
+            def value(self, value_: T) -> None:
+                self.ref.value = value_
+
+            @property
+            def left(self) -> SimpleBST.Node:
+                return self.ref.left
+
+            @left.setter
+            def left(self, node: SimpleBST.Node) -> None:
+                self.ref.left = node
+
+            @property
+            def right(self) -> SimpleBST.Node:
+                return self.ref.right
+
+            @right.setter
+            def right(self, node: SimpleBST.Node) -> None:
+                self.ref.right = node
+
+        cur = self
+        nxt_attr = 'root'
+        nxt = NodeRef()
+
+        while nxt.ref is not None:
+            if nxt.value == value:
+                if nxt.right is None:
+                    nxt.ref = nxt.left
+                    return
+
+                elif nxt.left is None:
+                    nxt.ref = nxt.right
+                    return
+
+                else:
+                    cur = target = nxt.ref
+                    nxt_attr = 'left'
+                    while nxt.left is not None:
+                        cur = nxt.ref
+                    target.value = nxt.value
+                    nxt.ref = nxt.right
 
                     return
 
             elif value < nxt.value:
-                cur = nxt
-                nxt = cur.left
+                cur = nxt.ref
+                nxt_attr = 'left'
             else:
-                cur = nxt
-                nxt = cur.right
+                cur = nxt.ref
+                nxt_attr = 'right'
 
-        raise KeyError
+        raise KeyError  # leaf node reached without finding value
