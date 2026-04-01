1e5c38bce7ff44d67b70a00d9f92ceca687bb512
Connor Gibson <connor@nextdoortech.net>
2026-03-31 22:53:25 -0700

    Implement BST

--- Labs/Lab 08-09/bst.py
+++ Labs/Lab 08-09/bst.py
@@ -1,4 +1,143 @@
-from typing import Self
+from typing import Self, Optional
+from collections.abc import MutableMapping, Generator
+
+
+class BST[KT, VT](MutableMapping[KT, VT]):
+    class Node(MutableMapping):
+        __slots__ = ('key', 'value', 'left', 'right')
+
+        def __init__(self, key: KT, value: VT, left: Self = None, right: Self = None) -> None:
+            self.key = key
+            self.value = value
+            self.left = left
+            self.right = right
+
+        def is_leaf(self) -> bool:
+            return self.left is None and self.right is None
+
+        def __getitem__(self, key: KT, /) -> VT:
+            if key is self.key or key == self.key:
+                return self.value
+            elif key < self.key and self.left is not None:
+                return self.left[key]
+            elif self.right is not None:
+                return self.right[key]
+            else:
+                raise KeyError(key)
+
+        def __setitem__(self, key: KT, value: VT) -> None:
+            if key is self.key or key == self.key:
+                self.value = value
+            elif key < self.key:
+                if self.left is not None:
+                    self.left[key] = value
+                else:
+                    self.left = self.__class__(key, value)
+            else:
+                if self.right is not None:
+                    self.right[key] = value
+                else:
+                    self.right = self.__class__(key, value)
+
+        class NoRightChildError(LookupError):
+            """Attempted to pop the least right child when the right child was None."""
+            pass
+
+        def pop_lrc(self) -> VT:
+            """pop least right child"""
+
+            if self.right is None:
+                raise self.NoRightChildError
+            else:
+                cur = self
+                nxt_attr = 'right'
+                nxt = getattr(cur, nxt_attr)
+
+                while nxt.left is not None:
+                    cur = nxt
+                    nxt_attr = 'left'
+                    nxt = getattr(cur, nxt_attr)
+
+                retval = nxt.value
+                setattr(cur, nxt_attr, nxt.right)
+                return retval
+
+        def __delitem__(self, key: KT, /) -> None:
+            if key is self.key or key == self.key:
+                raise RuntimeError(f"del {self.__class__.__qualname__}[{key}] cannot be called from Node to be deleted")
+            elif key < self.key and self.left is not None:
+                if key is self.left.key or key == self.left.key:
+                    try:
+                        self.left.value = self.left.pop_lrc()
+                    except self.NoRightChildError:
+                        self.left = self.left.left
+                else:
+                    del self.left[key]
+            elif self.right is not None:
+                if key is self.right.key or key == self.right.key:
+                    try:
+                        self.right.value = self.right.pop_lrc()
+                    except self.NoRightChildError:
+                        self.right = self.right.left
+                else:
+                    del self.right[key]
+            else:
+                raise KeyError(key)
+
+        def __len__(self) -> int:
+            _len = 1
+            if self.left is not None:
+                _len += len(self.left)
+            if self.right is not None:
+                _len += len(self.right)
+            return _len
+
+        def __iter__(self) -> Generator[VT, None, None]:
+            if self.left is not None:
+                yield from self.left
+            yield self.key
+            if self.right is not None:
+                yield from self.right
+
+    root: Optional[Node]
+
+    def __init__(self, root: Node = None) -> None:
+        self.root = root
+
+    def __getitem__(self, key: KT, /) -> VT:
+        if self.root is None:
+            raise KeyError(key)
+        else:
+            return self.root[key]
+
+    def __setitem__(self, key: KT, value: VT, /) -> None:
+        if self.root is None:
+            self.root = self.Node(key, value)
+        else:
+            self.root[key] = value
+
+    def __delitem__(self, key: KT, /) -> None:
+        if self.root is None:
+            raise KeyError(key)
+        elif key is self.root.key or key == self.root.key:
+            try:
+                self.root.value = self.root.pop_lrc()
+            except self.root.NoRightChildError:
+                self.root = self.root.left
+
+        else:
+            del self.root[key]
+
+    def __len__(self) -> int:
+        if self.root is not None:
+            return len(self.root)
+        else:
+            return 0
+
+    def __iter__(self) -> Generator[VT, None, None]:
+        if self.root is not None:
+            yield from self.root
+
 
 class SimpleBST[T]:
     class Node:
