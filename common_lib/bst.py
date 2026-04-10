54b1e88f927eb60d8370f402876042440dfba065
Connor Gibson <connor@nextdoortech.net>
2026-04-10 01:18:16 -0700

    Comment out SimpleBST

--- common_lib/bst.py
+++ common_lib/bst.py
@@ -411,117 +411,117 @@ class AVLMap[KT: Comparable, VT](BSTMap[KT, VT]):
         super().__delitem__(key)
 
 
-class SimpleNode[T]:
-    def __init__(self, value: T, left=None, right=None) -> None:
-        self.value: T = value
-        self.left: Self = left
-        self.right: Self = right
-
-
-class SimpleBST[T: Comparable]:
-    Node: type = SimpleNode
-
-    root: SimpleNode[T] | None
-
-    def __init__(self):
-        self.root = None
-
-    def search(self, value: T) -> T:
-        cur = self.root
-        while cur is not None:
-            if value == cur.value:
-                return cur.value
-            elif value < cur.value:
-                cur = cur.left
-            else:
-                cur = cur.right
-
-        raise KeyError
-
-    def insert(self, value: T) -> None:
-        cur = self.root
-        if cur is None:
-            self.root = self.Node(value)
-            return
-
-        while True:
-            if value < cur.value:
-                if cur.left is None:
-                    cur.left = self.Node(value)
-                    return
-                else:
-                    cur = cur.left
-            else:
-                if cur.right is None:
-                    cur.right = self.Node(value)
-                    return
-                else:
-                    cur = cur.right
-
-    def delete(self: Self, value: T) -> None:
-        cur: Self | Node = self
-        nxt_attr = 'root'
-
-        type Node = SimpleNode
-
-        class NodeRef:
-            @property
-            def ref(self) -> Node:
-                nonlocal cur, nxt_attr
-                return getattr(cur, nxt_attr)
-
-            @ref.setter
-            def ref(self, node: Node) -> None:
-                nonlocal cur, nxt_attr
-                setattr(cur, nxt_attr, node)
-
-            @property
-            def value(self) -> T:
-                return self.ref.value
-
-            @value.setter
-            def value(self, value_: T) -> None:
-                self.ref.value = value_
-
-            @property
-            def left(self) -> Node:
-                return self.ref.left
-
-            @left.setter
-            def left(self, node: Node) -> None:
-                self.ref.left = node
-
-            @property
-            def right(self) -> Node:
-                return self.ref.right
-
-            @right.setter
-            def right(self, node: Node) -> None:
-                self.ref.right = node
-
-        nxt = NodeRef()
-
-        while nxt.ref is not None:
-            if nxt.value == value:
-                if nxt.right is None:
-                    nxt.ref = nxt.left
-                    return
-
-                else:
-                    cur = target = nxt.ref
-                    nxt_attr = 'left'
-                    while nxt.left is not None:
-                        cur = nxt.ref
-                    target.value = nxt.value
-                    nxt.ref = nxt.right
-
-                    return
-
-            elif value < nxt.value:
-                cur = nxt.ref
-                nxt_attr = 'left'
-            else:
-                cur = nxt.ref
-                nxt_attr = 'right'
-
-        raise KeyError  # leaf node reached without finding value
+# class SimpleNode[T: Comparable]:
+#     def __init__(self, value: T, left: Self | None = None, right: Self | None = None) -> None:
+#         self.value: T = value
+#         self.left: Self | None = left
+#         self.right: Self | None = right
+#
+#
+# class SimpleBST[T: Comparable]:
+#     Node: ClassVar[type[SimpleNode]] = SimpleNode[T]
+#
+#     root: SimpleNode[T] | None
+#
+#     def __init__(self):
+#         self.root = None
+#
+#     def search(self, value: T) -> T:
+#         cur = self.root
+#         while cur is not None:
+#             if value == cur.value:
+#                 return cur.value
+#             elif value < cur.value:
+#                 cur = cur.left
+#             else:
+#                 cur = cur.right
+#
+#         raise KeyError
+#
+#     def insert(self, value: T) -> None:
+#         if self.root is None:
+#             self.root = SimpleNode(value)
+#             return
+#
+#         cur: SimpleNode[T] = self.root
+#         if cur is None:
+#
+#         while True:
+#             if value < cur.value:
+#                 if cur.left is None:
+#                     cur.left = SimpleNode(value)
+#                     return
+#                 else:
+#                     cur = cur.left
+#             else:
+#                 if cur.right is None:
+#                     cur.right = SimpleNode(value)
+#                     return
+#                 else:
+#                     cur = cur.right
+#
+#     def delete(self: Self, value: T) -> None:
+#         cur: Self | SimpleNode[T] = self
+#         nxt_attr = 'root'
+#
+#         class NodeRef:
+#             @property
+#             def ref(self) -> SimpleNode[T]:
+#                 nonlocal cur, nxt_attr
+#                 return getattr(cur, nxt_attr)
+#
+#             @ref.setter
+#             def ref(self, node: SimpleNode[T]) -> None:
+#                 nonlocal cur, nxt_attr
+#                 setattr(cur, nxt_attr, node)
+#
+#             @property
+#             def value(self) -> T:
+#                 return self.ref.value
+#
+#             @value.setter
+#             def value(self, value_: T) -> None:
+#                 self.ref.value = value_
+#
+#             @property
+#             def left(self) -> SimpleNode[T]:
+#                 return self.ref.left
+#
+#             @left.setter
+#             def left(self, node: SimpleNode[T]) -> None:
+#                 self.ref.left = node
+#
+#             @property
+#             def right(self) -> SimpleNode[T]:
+#                 return self.ref.right
+#
+#             @right.setter
+#             def right(self, node: SimpleNode[T]) -> None:
+#                 self.ref.right = node
+#
+#         nxt = NodeRef()
+#
+#         while nxt.ref is not None:
+#             if nxt.value == value:
+#                 if nxt.right is None:
+#                     nxt.ref = nxt.left
+#                     return
+#
+#                 else:
+#                     cur = target = nxt.ref
+#                     nxt_attr = 'left'
+#                     while nxt.left is not None:
+#                         cur = nxt.ref
+#                     target.value = nxt.value
+#                     nxt.ref = nxt.right
+#
+#                     return
+#
+#             elif value < nxt.value:
+#                 cur = nxt.ref
+#                 nxt_attr = 'left'
+#             else:
+#                 cur = nxt.ref
+#                 nxt_attr = 'right'
+#
+#         raise KeyError  # leaf node reached without finding value
