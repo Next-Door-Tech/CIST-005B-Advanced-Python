42bbdd51420f98fa46ba893e8403d91f30d44f2b
Connor Gibson <connor@nextdoortech.net>
2026-04-11 22:29:00 -0700

    Implement spell_checker.py

---
+++ Labs/Lab 11/spell_checker.py
@@ -0,0 +1,23 @@
+from bst import AVLSeq
+from itertools import chain
+
+dictionary: AVLSeq[str] = AVLSeq()
+
+with open("dictionary.txt", 'r') as d:
+    for line in d:
+        dictionary.add(line.lower())
+
+misspelled: AVLSeq[str] = AVLSeq()
+
+with open("check_me.txt", 'r') as f:
+    for word in chain(line.split() for line in f):
+        if word.lower() not in dictionary:
+            misspelled.add(word.lower())
+
+print("Misspelled words:")
+print('\t' + word for word in misspelled)
+print()
+
+print("Dictionary Tree:")
+for node in dictionary.root:
+    print(f"\t'{node.value}' skew: {node.skew}")
