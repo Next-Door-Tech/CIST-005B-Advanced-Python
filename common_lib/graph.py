a9aa980e190a371f90b319c4355c6c5a2e674aa5
Connor Gibson <connor@nextdoortech.net>
2026-05-11 14:14:44 -0700

    Create Weight protocol type

--- common_lib/graph.py
+++ common_lib/graph.py
@@ -1,6 +1,68 @@
 from collections.abc import Collection, Iterable, Hashable
 from typing import Protocol, Literal
 from abc import abstractmethod
+from typing import Protocol, Literal, runtime_checkable
+from numbers import Real
+
+
+@runtime_checkable
+class Weight(Protocol):
+    """Must support comparison, negation, and addition with other Weight types."""
+
+    @abstractmethod
+    def __eq__(self, other: Weight) -> bool:
+        """self == other"""
+        raise NotImplementedError
+
+    def __ne__(self, other: Weight) -> bool:
+        """self != other"""
+        return not self == other
+
+    @abstractmethod
+    def __lt__(self, other: Weight) -> bool:
+        """self < other"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __le__(self, other: Weight) -> bool:
+        """self <= other"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __gt__(self, other: Weight) -> bool:
+        """self > other"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __ge__(self, other: Weight) -> bool:
+        """self >= other"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __add__(self, other: Weight) -> Weight:
+        """self + other"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __radd__(self, other: Weight) -> Weight:
+        """other + self"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __neg__(self) -> Weight:
+        """-self"""
+        raise NotImplementedError
+
+    def __sub__(self, other: Weight) -> Weight:
+        """self - other"""
+        return self + -other
+
+    def __rsub__(self, other) -> Weight:
+        """other - self"""
+        return -self + other
+
+
+Weight.register(Real)
 
 
 class Edge[VertT: Hashable](Protocol):
