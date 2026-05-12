b9f96359bf5c3d7b3f524d1aa4f0420b89241267
Connor Gibson <connor@nextdoortech.net>
2026-05-12 13:30:40 -0700

    Simplify Weight Protocol

--- common_lib/graph.py
+++ common_lib/graph.py
@@ -1,67 +1,54 @@
 from collections.abc import Collection, Iterable, Hashable, Set
 from typing import Protocol, Literal, runtime_checkable, Self
 from abc import ABC, abstractmethod
-from numbers import Real
 
 
 @runtime_checkable
 class Weight(Protocol):
-    """Must support comparison, negation, and addition with other Weight types."""
+    """Must support comparison, addition, and negation/subtraction with other Weight types."""
 
     @abstractmethod
-    def __eq__(self, other: Weight) -> bool:
+    def __eq__(self, other: Weight | object) -> bool:
         """self == other"""
-        raise NotImplementedError
 
-    def __ne__(self, other: Weight) -> bool:
+    def __ne__(self, other: Weight | object) -> bool:
         """self != other"""
-        return not self == other
 
     @abstractmethod
     def __lt__(self, other: Weight) -> bool:
         """self < other"""
-        raise NotImplementedError
 
     @abstractmethod
     def __le__(self, other: Weight) -> bool:
         """self <= other"""
-        raise NotImplementedError
 
     @abstractmethod
     def __gt__(self, other: Weight) -> bool:
         """self > other"""
-        raise NotImplementedError
 
     @abstractmethod
     def __ge__(self, other: Weight) -> bool:
         """self >= other"""
-        raise NotImplementedError
 
     @abstractmethod
     def __add__(self, other: Weight) -> Weight:
         """self + other"""
-        raise NotImplementedError
 
     @abstractmethod
     def __radd__(self, other: Weight) -> Weight:
         """other + self"""
-        raise NotImplementedError
 
     @abstractmethod
     def __neg__(self) -> Weight:
         """-self"""
-        raise NotImplementedError
 
+    @abstractmethod
     def __sub__(self, other: Weight) -> Weight:
         """self - other"""
-        return self + -other
 
+    @abstractmethod
     def __rsub__(self, other) -> Weight:
         """other - self"""
-        return -self + other
-
-
-Weight.register(Real)
 
 
 class Edge[VertT: Hashable](Protocol):
